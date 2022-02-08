33c36dd18786d684181a24032b315623aeade64c
Shannonbyte <shannondaly0408@gmail.com>
2022-02-07 18:47:57 -0700

    removed some things

---
+++ to-do/src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component{
+  render(){
+    return (
+      <div>
+        <header className="Main-header">
+        <h1> To-Do List </h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Header;
